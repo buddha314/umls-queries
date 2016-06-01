21da665a82477b8dc44b70d57a7572fa5b0e6661
buddha314 <buddha_314@yahoo.com>
2016-06-01 10:39:49 -0700

    got roots and leaves

--- scripts/r/umls_hierarchy.R
+++ scripts/r/umls_hierarchy.R
@@ -7,22 +7,24 @@ chrel <- "child_rels.txt"
 # > cat /Users/buddha/src/nih/umls/data/2015AA/META/MRREL.RRF | grep -i "chd" > child_rels.txt
 #
 # In the 2015 release, this should be  2,615,420 records. Doing "parent" gives you 3,103,796 records.
-
+# Column names from this page: http://www.ncbi.nlm.nih.gov/books/NBK9685/table/ch03.T.related_concepts_file_mrrel_rrf/?report=objectonly
+ 
 d <- read.table(chrel, sep="|")
-
-# From this page: http://www.ncbi.nlm.nih.gov/books/NBK9685/table/ch03.T.related_concepts_file_mrrel_rrf/?report=objectonly
 names(d) <- c("cui1", "aui1", "stype1", "rel", "cui2", "aui2", "stype2"
 , "rela", "rui", "srui", "sab", "sl", "rg", "dir", "suppress", "cvf")
-
 dim(d)
-
-# iGraph wants the first two columns to be the nodes
-e <- data.frame(d$cui1, d$cui2, d$rel)
-
 # Now suppose you only want the relationships between the CUIs, I like to roll up over the AUIs.
-f <- unique(e)
-
-# Build the graph from f, which is only cui-cui
+f <- unique(data.frame(d$cui1, d$cui2, d$rel))
 g <- graph.data.frame(d = f, directed=TRUE)
+# Save some memory
+rm(f)
 
-# Now you can do the fun stuff!
+# Now, what you want, from this post: https://lists.nongnu.org/archive/html/igraph-help/2010-05/msg00093.html
+leaves <- V(g)[degree(g, mode="out")==0]
+roots <- V(g)[degree(g, mode="in")==0]
+
+length(leaves)
+length(roots)
+
+# What do you want to do next?
+
